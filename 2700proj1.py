3c5ecd6f2e01e5448bbdf5766e239f1ac714fd33
Lora Elliott <128410600+loraavie@users.noreply.github.com>
2024-03-02 19:57:44 -0600

    Update 2700proj1.py
    added some more functions and also example use cases for a few functions

--- 2700proj1.py
+++ 2700proj1.py
@@ -1,27 +1,58 @@
 """
-Use: for giving the answer to yes or no questions
+Project 1 CSC2700
+Names: Lora Elliott and Shubheksha Acharya
 """
-def y_or_n(inp):
 
-    if(inp.upper()=="YES" or inp.upper()=="Y"):
-        ans = True
-        return 5
-    elif(inp.upper()=="NO" or inp.upper()=="N"):
-        ans = False
-        return 0
-    else:
+#Small function to allow the program to gracefully terminate at will.
+def quit_on_demand():
+    print("You have chosen to exit the quiz. \n Now terminating...")
+    quit()
+
+#Use: for giving the answer to yes or no questions
+def y_or_n(inp):
+    while True:
+        if(inp.upper()=="YES" or inp.upper()=="Y"):
+            ans = True
+            return 5
+        elif(inp.upper()=="NO" or inp.upper()=="N"):
+            ans = False
+            return 0
+        elif(inp.upper()=="QUIT"):
+            quit_on_demand()
+
         inp = input("Invalid Input: try again")
         y_or_n(inp)
+
+'''
+Used for questions that have multiple answer choices. Takes a response of a, b, or c
+'''
+def options(opt):
+     while True:
+        if opt.upper()=="A":
+            return 'a'
+        elif opt.upper()=="B":
+            return 'b'
+        elif opt.upper()=="C":
+            return 'c'
+        elif opt.upper()=="QUIT":
+            quit_on_demand()
 
 """
 Use: for giving the answer to how much questions 
 ex: 'how often do you do this' 'how much does this apply to you'
 ideal input is a scale 1-5
+q: represents the question you want to ask
 """
-def how_much(inp):
+def how_much(q):
+    print(q)
     while True:
         try:
-            return int(inp)
+            inp = int(input("Enter an integer input from 1-5:"))
+            if inp>=1 and inp<6:
+                return inp
+            else:
+                print("Invalid input. Now terminating...")
+                quit_on_demand()
         except:
             input("Your input was not a number. Try again.")
 
@@ -29,21 +60,22 @@ def how_much(inp):
 inp = 5. Can either be modified to have questions directly inside each case or """
 def game_loop(inp):
 
-    res1 = 0
-    res2 = 0
-    res3 = 0
+    res1 = 1
+    res2 = 1
+    res3 = 1
     x = 0
     while x<inp:
         match x:
             case 0:
-                print("question 1")
-                inp= how_much(input())
-                res1= res1+inp
+                print("Question 1")
+                q = "How often would you say that you find yourself lost in thought or distracted by your surroundings?\n"
+                q+= "Please enter a number from 1-5, with 1 meaning never and 5 meaning all the time."
+                inp= how_much(q)
+                res2= res1+inp
                 print(inp)
-
-
             case 1:
-                print("question 2")
+                q= "Would you consider yourself to be a strong leader? Type y or n to answer.\n"
+                res1 += y_or_n(input(q))
             case 2:
                 print("question 3")
             case 3:
@@ -59,9 +91,22 @@ def game_loop(inp):
         ending3()
 
 def ending1():
-    print("This is ending one. This means...")
+    print("Blossom Ending")
 def ending2():
-    print("This is ending two. This means...")
+    print("Bubbles Ending")
 def ending3():
-    print("This is ending three. This means...")
-game_loop(4)
+    print("Buttercup Ending")
+
+
+print("Welcome to our personality quiz. Answer a few questions to find out which Powerpuff Girl you are.")
+print("To end the game, enter quit at any time.")
+
+game_loop(5)
+while True:
+    play_again = input("If you would like to take the quiz again, enter y. \nEnter anything else to exit.\n")
+    if play_again == "y" or play_again == "Y":
+        game_loop(5)
+    else:
+        print("Thank you for playing! Goodbye.")
+        quit()
+
